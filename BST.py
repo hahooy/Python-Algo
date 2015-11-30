f95397f51053c2c777c9876c9a36bd96be78fd75
Yongzheng Huang <iamhuangyz@gmail.com>
2015-11-30 00:25:04 -0600

    added binary search tree

---
+++ BST.py
@@ -0,0 +1,60 @@
+class BST:
+    class Node:
+        def __init__(self, value, left, right):
+            self.value = value
+            self.left = left
+            self.right = right
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        self.root = self.__insert_helper(self.root, value)
+
+    def __insert_helper(self, root, value):
+        if root == None:
+            return self.Node(value, None, None)
+        if value < root.value: # go left
+            root.left = self.__insert_helper(root.left, value)
+        elif value > root.value: # go right
+            root.right = self.__insert_helper(root.right, value)
+        return root
+
+    def find(self, value):
+        return self.__find_helper(self.root, value)
+    
+    def __find_helper(self, root, value):
+        if root == None:
+            return False
+        elif root.value > value:
+            return self.__find_helper(root.left, value)
+        elif root.value < value:
+            return self.__find_helper(root.right, value)
+        else:
+            return True
+
+    def traverse(self, root, s):
+        if root == None:
+            return s
+        s = self.traverse(root.left, s)
+        s = s + " " + str(root.value)
+        s = self.traverse(root.right, s)
+        return s
+
+    def __str__(self):
+        s = self.traverse(self.root, "")
+        return s
+
+if __name__ == "__main__":
+    tree = BST()
+    tree.insert(5)
+    tree.insert(1)
+    tree.insert(6)
+    tree.insert(3)
+    tree.insert(-11)
+    tree.insert(-222)
+    tree.insert(-222)
+    tree.insert(-225)
+    print(tree)
+    print(tree.find(-11))
+    print(tree.find(2))
